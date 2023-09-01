53166280eee2adb50586272d3a00fe457cacf3c6
abdul <abdulsk054@gmail.com>
2023-09-01 10:09:47 +0530

    terraform

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "sg_ingress_cidr" {}
+variable "elasticache_type" {}
+variable "family" {}
+variable "port" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
